753bc5ec2d716aea331d4dec5164de63eb0c06cd
Hamidreza Mohamadi <hamid.reactjs@gmail.com>
2024-04-06 12:38:20 +0330

    end version 1.0.0

--- README.md
+++ README.md
@@ -1,8 +1,39 @@
-# React + Vite
+# BMI Project
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+## Demo
 
-Currently, two official plugins are available:
+- [Project-demo](https://hamid-bmi.vercel.app/)
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+This project is a BMI (Body Mass Index) and body fat percentage calculator implemented as a web application. It allows users to input their weight, height, and other metrics to calculate and interpret body composition metrics.
+
+## Overview
+
+The BMI/Fat Calculator is designed to help users quickly assess their body mass index (BMI) and estimate body fat percentage based on commonly used formulas and metrics. The calculator provides an intuitive interface for entering data and viewing results, along with interpretations of the calculated values.
+
+## Key Features
+
+- **BMI Calculation**: Calculate BMI based on weight (in kilograms) and height (in meters).
+- **Body Fat Percentage Estimation**: Estimate body fat percentage using additional metrics (e.g., gender, age, waist circumference).
+- **Interactive Interface**: User-friendly interface with input fields and result display.
+- **Result Interpretation**: Display BMI classifications and body fat percentage ranges based on established guidelines.
+- **Responsive Design**: Support for various devices and screen sizes for optimal user experience.
+- **Customization**: Easily customizable and extensible to accommodate additional features or refinements.
+
+## Usage
+
+To use the BMI/Fat Calculator:
+
+1. Enter your weight (in kilograms) and height (in meters) in the provided input fields.
+2. Optionally, input additional metrics (e.g., gender, age) for body fat percentage estimation.
+3. Click the "Calculate" button to generate results.
+4. Review the displayed BMI value and body fat percentage along with interpretations.
+
+## Getting Started
+
+To run the BMI/Fat Calculator locally:
+
+1. Clone the repository:
+
+   ```bash
+   git clone https://github.com/hamid-js/bmi-calculator
+   ```
--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,8 @@ function reducer(state, action) {
       return { ...state, sliderIndex: action.payload };
     case 'SET_FAT':
       return { ...state, fat: action.payload };
+      case 'SET_SELECTED':
+      return { ...state, selected: action.payload };
     default:
       return state;
   }
@@ -24,11 +26,12 @@ function App() {
     '/images/boy1.png',
   ];
   const [state, dispatch] = useReducer(reducer, initialState);
-  const { sliderIndex, fat } = state;
+  const { sliderIndex, fat ,selected  } = state;
+  console.log("App ~ selected:", selected)
 
   function changeFat(e) {
     const newFat = e.target.value;
-
+    const newSelected = false;
     let newSliderIndex = 3;
     if (newFat >= 35) {
       newSliderIndex = 0;
@@ -37,36 +40,52 @@ function App() {
     } else if (newFat > 15 && newFat < 25) {
       newSliderIndex = 2;
     }
-
+    dispatch({ type: 'SET_SELECTED', payload: newSelected });
     dispatch({ type: 'SET_SLIDER_INDEX', payload: newSliderIndex });
     dispatch({ type: 'SET_FAT', payload: newFat });
   }
 
-  function changeIndex(index) {
-    let newFat = 5;
-    switch (index) {
+  function changeIndex(action) {
+    const newSelected = false;
+    dispatch({ type: 'SET_SELECTED', payload: newSelected });
+    let newFat = fat;
+    let newIndex = sliderIndex;
+
+    if (action === 'right') {
+      newIndex = newIndex === 0 ? sliderImages.length - 1 : newIndex - 1;
+    } else if (action === 'left') {
+      newIndex = (newIndex + 1) % sliderImages.length;
+    }else {
+   newIndex = action
+    }
+
+    switch (newIndex) {
       case 0:
-        newFat = 36;
+        newFat = 35;
         break;
       case 1:
-        newFat = 26;
+        newFat = 25;
         break;
       case 2:
-        newFat = 16;
+        newFat = 15;
         break;
       default:
         newFat = 5;
-        break;
     }
 
-    dispatch({ type: 'SET_SLIDER_INDEX', payload: index });
+    dispatch({ type: 'SET_SLIDER_INDEX', payload: newIndex });
     dispatch({ type: 'SET_FAT', payload: newFat });
+  }
+
+  function toggleCheckbox() {
+    const newSelected = !selected;
+    dispatch({ type: 'SET_SELECTED', payload: newSelected });
   }
 
   return (
     <div className="h-[140svh] bg-slate-900  pt-10 text-stone-300">
-      <div className="mx-auto h-[110svh] w-4/5 rounded-md bg-zinc-700 px-2 text-sm">
-        <header>
+      <div className="mx-auto h-fit w-4/5 rounded-md bg-zinc-700 px-2 pb-10 pt-2 text-sm sm:w-3/5 md:w-2/5 lg:w-2/6">
+        <header className="rounded-md border-t border-zinc-600 p-1  shadow-sm  shadow-zinc-600">
           <div className="pt-1">
             <img className="m-3 h-5 w-5" src="/images/menu.png" alt="" />
           </div>
@@ -75,33 +94,40 @@ function App() {
           </ul>
         </header>
 
-        <div className="mx-1 my-4">
+        <div className="mx-1 my-4 p-2  shadow-sm shadow-slate-600">
           <p>نزدیک ترین تصویر را به بدن خود انتخاب کنید</p>
         </div>
 
-        <section className="h-[180px] w-full">
+        <section className="h-[170px] w-full">
           <div
             style={{ backgroundImage: `url(${sliderImages[sliderIndex]})` }}
             className="relative h-full w-full bg-contain bg-center bg-no-repeat duration-500"
           >
             <div className="text-lime-400 *:absolute *:top-16 *:cursor-pointer  *:border-none *:text-6xl *:2xl:text-9xl">
-              <button className=" left-2 ">&#8250;</button>
-              <button className="right-2  ">&#8249;</button>
+              <button onClick={() => changeIndex('left')} className=" left-2 ">
+                &#8250;
+              </button>
+              <button
+                onClick={() => changeIndex('right')}
+                className="right-2  "
+              >
+                &#8249;
+              </button>
             </div>
           </div>
         </section>
 
-        <div>
-          <label htmlFor="">
-            <input
-              type="radio"
-              className="mx-auto my-3 block rounded-full checked:text-lime-300"
-            />
-          </label>
+        <div className="flex items-center justify-center mb-8">
+          <div>
+            <label className="container">
+              <input onChange={toggleCheckbox} value={selected} checked={selected} type="checkbox" name="checkbox" id="m" />
+              <span className="checkmark"></span>
+            </label>
+          </div>
         </div>
 
-        <div className="w-1/1 mx-auto text-center">
-          <p>{fat}</p>
+        <div className="w-full  mx-auto text-center">
+          <p className='pr-2 text-lg text-lime-200'>{fat}</p>
           <input
             onChange={changeFat}
             dir="ltr"
@@ -114,7 +140,7 @@ function App() {
         </div>
 
         <div className="relative my-7 ">
-          <ul className=" absolute left-[50%] top-0 flex -translate-x-[50%] gap-3 duration-1000 *:h-2 *:w-2 *:rounded-full *:bg-white sm:top-[25rem] sm:gap-4  md:top-[29rem] md:gap-5 ">
+          <ul className=" absolute left-[50%] top-0 flex  -translate-x-[50%] gap-3 duration-1000 *:h-2 *:w-2 *:rounded-full *:bg-white sm:top-0  sm:gap-5   ">
             {sliderImages.map((url, index) => (
               <li
                 className="duration-500"
@@ -129,7 +155,9 @@ function App() {
           </ul>
         </div>
 
-        <div className="mx-auto mt-20 flex h-8 w-4/5 items-center justify-center border-t border-lime-300 bg-zinc-700 shadow-sm shadow-lime-300">
+        <div 
+        style={{backgroundColor:`${selected ? "#a3e635" : ""}`,
+        color:`${selected ? "#3f3f46" : ""}`}} className="mx-auto mt-20 flex h-8 w-4/5 items-center justify-center font-semibold border-t border-lime-300 bg-zinc-700 shadow-sm  shadow-lime-300">
           <span>تایید و ادامه</span>
         </div>
       </div>
